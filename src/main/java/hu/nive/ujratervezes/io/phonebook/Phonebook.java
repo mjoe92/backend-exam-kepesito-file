2e68af53e3cace247751ef960a65f8a542d2ebe6
Jozsef Csurgai <56649112+mjoe92@users.noreply.github.com>
2021-04-29 11:39:28 +0200

    Done!

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,23 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Map;
+
+public class Phonebook {
+
+    public void exportPhonebook(Map<String, String> contacts, String output) throws IOException {
+
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        }
+
+        Writer writer = new FileWriter(output);
+        for (Map.Entry<String, String> line : contacts.entrySet()) {
+            writer.write(line.getKey() + ": " + line.getValue() + "\n");
+        }
+        writer.close();
+    }
+
+}
